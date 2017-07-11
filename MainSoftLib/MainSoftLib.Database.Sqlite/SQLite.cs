411e8ad9a6b0187f0d2bf9b809b198601ac6ef5c
JURGEN <JURGEN@JURGEN-HP>
2017-07-11 11:33:01 -0500

    ok

---
+++ MainSoftLib/MainSoftLib.Database.Sqlite/SQLite.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainSoftLib.Database.Sqlite
+{
+    public class SQLite
+    {
+        string connectionString;
+
+        public SQLite(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataSet GetDataSet(string query)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+                    {
+                        SQLiteDataAdapter data = new SQLiteDataAdapter(cmd);
+
+                        DataSet ds = new DataSet();
+                        data.Fill(ds);
+
+                        cmd.Dispose();
+                        connection.Close();
+
+                        return ds;
+                    }                    
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;        
+            }
+        }
+
+        public DataTable GetDataTable(string query)
+        {
+            try
+            {
+                DataSet ds = GetDataSet(query);
+
+                if (ds != null && ds.Tables.Count > 0)
+                {
+                    return ds.Tables[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public string ExecuteNonQuery(string query)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+                    {
+                        int resp = cmd.ExecuteNonQuery();
+                        connection.Close();
+
+                        return "OK:" + resp.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "ERROR:" + ex.Message;
+            }
+        }
+
+        public string ExecuteExcalar(string query)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+                    {                        
+                        object resp = cmd.ExecuteScalar();
+                        connection.Close();
+
+                        return (resp == null ? "OK" : "OK:" + resp.ToString());
+                    }                    
+                }
+            }
+            catch (Exception ex)
+            {
+                return "ERROR:" + ex.Message;                
+            }
+        }
+
+        
+    }
+}
--- MainSoftLib/MainSoftLib/Form1.cs
+++ MainSoftLib/MainSoftLib/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MainSoftLib.Database.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,24 @@ namespace MainSoftLib
 {
     public partial class Form1 : Form
     {
+        static string DbRuta = "Data Source=H:\\prueba.db; Version=3; New=True; Compress=True;";
+
+        SQLite MiDb = new SQLite(DbRuta);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string consulta = "select * from ciudad";
+            string creacion = "CREATE TABLE ciudad3 (codigo VARCHAR(2) PRIMARY KEY, nombre VARCHAR(30));";
+
+            //DataSet ds = MiDb.GetDataSet(consulta);
+
+           string Resp = MiDb.ExecuteNonQuery(creacion);
+
         }
     }
 }
