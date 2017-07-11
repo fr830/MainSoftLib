bb2d8dfee325d3e729d54c6e796af4c419a05c4b
JURGEN <JURGEN@JURGEN-HP>
2017-07-11 13:46:00 -0500

    OK

--- MainSoftLib/MainSoftLib.Lib/Properties/AssemblyInfo.cs
+++ MainSoftLib/MainSoftLib.Lib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("MainSoftLib")]
+[assembly: AssemblyTitle("MainSoftLib.Database.MySql")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MainSoftLib")]
+[assembly: AssemblyProduct("MainSoftLib.Database.MySql")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
